6e65b1a1b2831ede77c367dd0c85338c6dfd898e
juanzini <juanzini@gmail.com>
2018-09-06 21:58:07 -0300

    refactor de los fereignKey

--- alumno/models.py
+++ alumno/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
  
 class Alumno(models.Model):
     numero_registro = models.PositiveIntegerField(primary_key = True)
-    carrera = models.ForeignKey('Carrera', on_delete = models.CASCADE)
+    carrera = models.ForeignKey('Carrera', on_delete = models.DO_NOTHING)
     mail = models.EmailField(unique = True)
     curriculum = models.FileField(upload_to='curriculums/')
     descripcion_intereses = models.TextField(max_length = 500)
@@ -25,7 +25,7 @@ class Carrera(models.Model):
     departamento = models.ForeignKey('Departamento', on_delete = models.CASCADE)
     nombre = models.CharField(max_length = 100)
     duracion = models.PositiveSmallIntegerField()
-    super_usuario = models.ForeignKey('SuperUsuario', on_delete = models.CASCADE)
+    super_usuario = settings.AUTH_USER_MODEL
     class Meta:
     	unique_together = (("departamento", "nombre"),)
 
@@ -45,31 +45,31 @@ class Docente(models.Model):
     numero_registro = models.PositiveIntegerField(primary_key = True)
     nombre = models.CharField(max_length = 30)
     apellido = models.CharField(max_length = 20)
-    departamento = models.ForeignKey('Departamento', on_delete = models.CASCADE)
+    departamento = models.OneToOneField('Departamento', on_delete = models.DO_NOTHING, null = True)
     mail = models.EmailField(unique = True)
     box_oficina = models.CharField(max_length = 30)
  
 class Pasantia(models.Model):
     fecha_inicio = models.DateTimeField(auto_now_add = True)
     fecha_fin = models.DateTimeField()
-    tutor_docente = models.ForeignKey('Docente', on_delete = models.CASCADE)
-    tutor_empresa = models.ForeignKey('TutorEmpresa', on_delete = models.CASCADE)
-    entrevista = models.OneToOneField('Entrevista', on_delete = models.CASCADE, primary_key = True)
+    tutor_docente = models.ForeignKey('Docente', on_delete = models.DO_NOTHING)
+    tutor_empresa = models.ForeignKey('TutorEmpresa', on_delete = models.DO_NOTHING)
+    entrevista = models.OneToOneField('Entrevista', on_delete = models.DO_NOTHING, primary_key = True)
     informe = models.FileField(upload_to='informes/')
     numero_legajo = models.PositiveIntegerField(unique = True, null=True)
     comentarios_empresa = models.TextField(max_length = 1000)
     comentarios_comision_pps = models.TextField(max_length = 1000)
  
 class TutorEmpresa(models.Model):
-    empresa = models.ForeignKey('Empresa', on_delete = models.CASCADE)
+    empresa = models.ForeignKey('Empresa', on_delete = models.DO_NOTHING)
     nombre = models.CharField(max_length = 30)
     apellido = models.CharField(max_length = 20)
     cargo = models.CharField(max_length = 30)
     mail = models.EmailField(primary_key = True)
  
 class Entrevista(models.Model):
-    alumno = models.ForeignKey('Alumno', on_delete = models.CASCADE)
-    empresa = models.ForeignKey('Empresa', on_delete = models.CASCADE)
+    alumno = models.ForeignKey('Alumno', on_delete = models.DO_NOTHING)
+    empresa = models.ForeignKey('Empresa', on_delete = models.DO_NOTHING)
     fecha = models.DateTimeField()
     resultado = models.TextField(max_length = 1000)
     comentarios_empresa = models.TextField(max_length = 1000)
@@ -103,7 +103,10 @@ class Postulaciones(models.Model):
 
 class Departamento(models.Model):
     nombre = models.CharField(max_length = 50, primary_key = True)
-    super_usuario = models.ForeignKey('SuperUsuario', on_delete = models.CASCADE)
+    super_usuario = settings.AUTH_USER_MODEL
 
-class SuperUsuario(models.Model):
-	user = settings.AUTH_USER_MODEL
+class DirectorDepartamento(models.Model):
+	departamento = models.ForeignKey('Departamento', on_delete = models.CASCADE)
+	docente = models.ForeignKey('Docente', on_delete = models.CASCADE)
+	class Meta:
+		unique_together = (("departamento", "docente"),)
