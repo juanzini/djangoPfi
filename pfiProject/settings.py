c4be78ea1da9778b729b3a94ac55d8aa6339f1ce
juanzini <juanzini@gmail.com>
2018-09-24 20:30:07 -0300

    avance hasta formularios

--- pfiProject/settings.py
+++ pfiProject/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Buenos_Aires'
 
