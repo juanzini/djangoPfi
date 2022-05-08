3734d77fc8e959f23de8388f39965e33a387683a
root <root@VM-Practica.unsl.edu.ar>
2022-05-08 18:48:44 -0300

    save production

--- pfiProject/settings.py
+++ pfiProject/settings.py
@@ -224,8 +224,9 @@ CELERY_TASK_SERIALIZER = 'json'
 ADMIN_MEDIA_PREFIX = '/media/'
 PRIVATE_STORAGE_ROOT = os.path.join(BASE_DIR, 'media/')
 
-EMAIL_USE_SSL = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dirinfo.spypp@gmail.com'
-EMAIL_HOST_PASSWORD = 'spypp2019jaz'
+EMAIL_HOST_PASSWORD = 'yqslizutooeoashy'
+EMAIL_USE_TLS = True
